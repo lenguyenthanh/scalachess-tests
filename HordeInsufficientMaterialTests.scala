9d89a99d2183447e0f092b51469871bc63dd6900
Thanh Le <lenguyenthanh@hotmail.com>
2023-03-23 13:01:09 +0100

    Use weaver instead of munit

--- HordeInsufficientMaterialTests.scala
+++ HordeInsufficientMaterialTests.scala
@@ -2,6 +2,8 @@
 //> using lib "org.typelevel::toolkit::0.0.4"
 //> using repository "https://raw.githubusercontent.com/lichess-org/lila-maven/master"
 //> using lib "org.lichess::scalachess:14.6.3"
+//> using lib "com.disneystreaming::weaver-cats:0.8.1"
+//> using testFramework "weaver.framework.CatsEffect"
 
 import cats.effect.{ IO, IOApp }
 import cats.syntax.all.*
@@ -9,7 +11,7 @@ import cats.effect.syntax.all.*
 import fs2.*
 import fs2.io.file.Files
 
-import munit.CatsEffectSuite
+import weaver.*
 
 import chess.format.Fen
 import chess.format.EpdFen
@@ -17,10 +19,11 @@ import chess.variant.*
 
 import Common.given
 
-class HordeInsufficientMaterialTests extends CatsEffectSuite:
+object HordeInsufficientMaterialTests extends SimpleIOSuite:
 
   test("insufficient material tests") {
-    run("resources/horde_insufficient_material.csv", Horde).assert
+    run("resources/horde_insufficient_material.csv", Horde)
+      .map(assert(_))
   }
 
   private def run(file: String, variant: Variant): IO[Boolean] =

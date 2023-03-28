05880cda6a6c6a110a842e6593ca5b339b725dca
Thanh Le <lenguyenthanh@hotmail.com>
2023-03-28 21:05:09 +0200

    Use project.scala

--- gen/RandomPerftTests71.scala
+++ gen/RandomPerftTests71.scala
@@ -1,9 +1,3 @@
-//> using scala "3.2.2"
-//> using repository "https://raw.githubusercontent.com/lichess-org/lila-maven/master"
-//> using lib "org.lichess::scalachess:14.6.3"
-//> using lib "com.disneystreaming::weaver-cats:0.8.1"
-//> using testFramework "weaver.framework.CatsEffect"
-
 import weaver.*
 
 import chess.format.EpdFen
---
+++ project.scala
@@ -0,0 +1,7 @@
+//> using scala "3.2.2"
+//> using repository "https://raw.githubusercontent.com/lichess-org/lila-maven/master"
+//> using lib "org.lichess::scalachess:14.6.3"
+//> using lib "org.typelevel::cats-parse:0.3.7"
+//> using lib "com.lihaoyi::os-lib:0.9.1"
+//> using lib "com.disneystreaming::weaver-cats:0.8.1"
+//> using testFramework "weaver.framework.CatsEffect"
